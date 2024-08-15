e981508f2042da8765dca4e534baa81550eb0100
igor-rizak <igor.rizak@ukr.net>
2024-08-15 16:48:42 +0300

    last

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -73,3 +73,7 @@ export const App = () => {
     </div>
   );
 };
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
-      </ThemeProvider>
+        <App />
+    </ThemeProvider>
   </React.StrictMode>
   
 );
